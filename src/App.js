a62a94a17adb3244abd5bb37bf327d4f79bc74d5
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-31 17:30:37 +0000

    Include dragons page route in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Navigation from './components/Navigation';
+import Dragons from './pages/Dragons';
 import Missions from './pages/Missions';
 import Profile from './pages/Profile';
 import Rockets from './pages/Rockets';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <BrowserRouter>
         <Navigation />
         <Routes>
           <Route path="/rockets" element={<Rockets />} />
           <Route path="/missions" element={<Missions />} />
+          <Route path="/dragons" element={<Dragons />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
