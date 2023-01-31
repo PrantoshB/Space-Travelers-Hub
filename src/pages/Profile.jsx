d4d3a8d382b5b7f637caabdc1cc107a8cb50d888
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-31 10:03:26 +0000

    Fix eslint err

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Profile = () => {
   return (
@@ -6,4 +6,4 @@ const Profile = () => {
   )
 }
 
-export default Profile
+export default Profile;
