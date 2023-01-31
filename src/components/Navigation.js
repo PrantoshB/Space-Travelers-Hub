973f6fac20113b417bc14d6f8d3c4e433b5c571c
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-31 10:00:52 +0000

    Modify Navigation.js file to include profile nav-link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/planet.png';
+import './Navigation.css';
 
 const Navigation = () => (
-  <div className="navigation" />
+  <nav className="navigation">
+    <div className="logo-wrapper">
+      <img className="logo" src={logo} alt="a planet" />
+      <span> Space Travelers&apos; Hub</span>
+    </div>
+    <ul>
+      <li>
+        <NavLink
+          to="/profile"
+          className={({ isActive }) => (isActive ? 'active' : 'link')}
+        >
+          My Profile
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Navigation;
