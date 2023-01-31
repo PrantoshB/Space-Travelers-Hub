77b9028d9549e7d2a74787cfcd69c7513a307086
PrantoshB <pbiswas1007@gmail.com>
2023-01-31 13:52:40 +0530

    Setup project files and add dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <h1>Space Travellers Hub</h1>
+      <Navigation />
+      <h1>Space Travelers Hub</h1>
     </div>
   );
 }
