b4244da386bb7946b81e26aa2e080f2cf5f6781d
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-31 09:58:34 +0000

    Install dependencies

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Profile from './pages/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navigation = () => (
+  <div className="navigation" />
+);
+
+export default Navigation;
