cee580ea611e92f9756cd7123019dd132f6a7ab6
PrantoshB <Pbiswas1007@gmail.com>
2023-01-31 21:00:00 +0530

    Fetch data from Rockets endpoint

---
+++ src/redux/rockets/rocket.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const FETCH_ROCKETS = ' FETCH_ROCKETS';
+const initialState = [];
+const url = 'https://api.spacexdata.com/v3/rockets';
+
+export const fetchRockets = createAsyncThunk(
+  FETCH_ROCKETS,
+  async (post, { dispatch }) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const rockets = data.map((item) => ({
+      id: item.rocket_id,
+      name: item.rocket_name,
+      type: item.rocket_type,
+      image: item.flickr_images[0],
+    }));
+    dispatch({
+      type: FETCH_ROCKETS,
+      payload: rockets,
+    });
+  },
+);
+
+const rocketReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_ROCKETS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default rocketReducer;
