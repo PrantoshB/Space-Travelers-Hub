0a17381413f67fdd869ecb2a0216fa9d26b5b32c
PrantoshB <Pbiswas1007@gmail.com>
2023-02-01 22:11:56 +0530

    Add My Rockets Section

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './Profile.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rocket';
 
-const Profile = () => <div>Profile</div>;
+const Profile = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.rockets);
+  const { status, rockets } = data;
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchRockets());
+    }
+  }, [dispatch, status]);
+  return (
+    <div className="profile-container">
+      <div className="mymissions">
+        <h2>My Missions</h2>
+        <table>
+          <tbody className="profile-tbody">
+            {
+              rockets.filter((rocket) => (
+                rocket.reserved === true
+              )).map((rocket) => (
+                <tr className="profile-tr" key={rocket.id}>
+                  <td className="profile-td">{rocket.name}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+      <div className="myrockets">
+        <h2>My Rockets</h2>
+      </div>
+      <div className="mydragons">
+        <h2>My Dragons</h2>
+      </div>
+    </div>
+  );
+};
 export default Profile;
