21c0ed2248bed81d1eafa45ff9bebd6c75f7e6cc
PrantoshB <Pbiswas1007@gmail.com>
2023-02-01 13:48:06 +0530

    Show rockets from fetched data

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Rocket from '../components/Rocket';
+import { fetchRockets } from '../redux/rockets/rocket';
 
-const Rockets = () => (
-  <>
-    <Rocket />
-  </>
-);
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rocket);
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+  return (
+    <>
+      {rockets.map((rocket) => (
+        <Rocket
+          image={rocket.image}
+          key={rocket.id}
+          title={rocket.name}
+          description={rocket.description}
+        />
+      ))}
+    </>
+
+  );
+};
 
 export default Rockets;
