87f793beb2409c02b5278d78fcee3651c8f6d8b6
PrantoshB <Pbiswas1007@gmail.com>
2023-02-02 18:34:47 +0530

    Add test for getAllMissions function

---
+++ src/_test_/functions/getMissions.test.js
@@ -0,0 +1,10 @@
+import missionsService from '../../services/missions'
+
+describe('Get Missions', () => {
+    test('gets the correct data', async () => {
+        const data = await missionsService.getAllMissions();
+        expect(data).toBeInstanceOf(Array);
+        expect(data[0]).toBeInstanceOf(Object);
+        expect(data[0].id).not.toBeNull();
+    });
+});
