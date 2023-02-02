74f1d00c8e0e2d04fdbe5b91516296f3cd5f5887
asifaminisonline <asifaminisonline@gmail.com>
2023-02-02 15:22:50 +0530

    Add reserved dragon action

--- src/redux/dragons/dragons.js
+++ src/redux/dragons/dragons.js
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import dragonsService from '../../services/dragons';
 
 const FETCH_DRAGONS = 'FETCH_DRAGONS';
+const RESERVE_DRAGON = 'RESERVE_DRAGON';
 const initialState = {
   status: 'idle',
   dragons: [],
@@ -15,14 +16,34 @@ const dragonsReducer = (state = initialState, action) => {
         status: 'succeeded',
         dragons: action.payload,
       };
+    case RESERVE_DRAGON:
+      return {
+        ...state,
+        dragons: action.payload,
+      };
     default:
       return state;
   }
 };
 
-export const getDragonsAsync = createAsyncThunk(FETCH_DRAGONS, async (arg, thunkAPI) => {
-  const payload = await dragonsService.getAllDragons();
-  thunkAPI.dispatch({ type: FETCH_DRAGONS, payload });
-});
+export const getDragonsAsync = createAsyncThunk(
+  FETCH_DRAGONS,
+  async (arg, thunkAPI) => {
+    const payload = await dragonsService.getAllDragons();
+    thunkAPI.dispatch({ type: FETCH_DRAGONS, payload });
+  },
+);
 
+export const reserveDragon = (id) => (dispatch, getState) => {
+  const state = getState().dragons.dragons;
+  const newState = state.map((dragon) => {
+    if (dragon.id !== id) return dragon;
+    const reserved = !dragon.reserved;
+    return { ...dragon, reserved };
+  });
+  dispatch({
+    type: RESERVE_DRAGON,
+    payload: newState,
+  });
+};
 export default dragonsReducer;
