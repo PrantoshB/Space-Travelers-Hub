2daeae7b62d3c11106000eb2381303132c19d2ea
PrantoshB <Pbiswas1007@gmail.com>
2023-02-02 18:35:49 +0530

    Fix Linters Errors

--- src/_test_/functions/getDragons.test.js
+++ src/_test_/functions/getDragons.test.js
@@ -1,10 +1,10 @@
-import dragonsService from "../../services/dragons";
+import dragonsService from '../../services/dragons';
 
 describe('Get Dragons', () => {
-    test('gets the correct data', async () => {
-        const data = await dragonsService.getAllDragons();
-        expect(data).toBeInstanceOf(Array);
-        expect(data[0]).toBeInstanceOf(Object);
-        expect(data[0].id).not.toBeNull();
-    });
+  test('gets the correct data', async () => {
+    const data = await dragonsService.getAllDragons();
+    expect(data).toBeInstanceOf(Array);
+    expect(data[0]).toBeInstanceOf(Object);
+    expect(data[0].id).not.toBeNull();
+  });
 });
--- src/_test_/functions/getRockets.test.js
+++ src/_test_/functions/getRockets.test.js
@@ -1,10 +1,10 @@
-import getRockets from "../../services/rockets";
+import getRockets from '../../services/rockets';
 
 describe('Get Rockets', () => {
-    test('gets the correct data', async () => {
-        const data = await getRockets();
-        expect(data).toBeInstanceOf(Array);
-        expect(data[0]).toBeInstanceOf(Object);
-        expect(data[0].id).not.toBeNull();
-    });
+  test('gets the correct data', async () => {
+    const data = await getRockets();
+    expect(data).toBeInstanceOf(Array);
+    expect(data[0]).toBeInstanceOf(Object);
+    expect(data[0].id).not.toBeNull();
+  });
 });
